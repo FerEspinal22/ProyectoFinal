69bc8c24352bbab0faf7808cb5d0a2663fa194f0
FerEspinal22 <ferespinal2001@gmail.com>
2021-11-25 17:23:05 -0600

    Primer avance
    Creación del menú del login

---
+++ ProyectoGrupo4/ProyectoGrupo4/Vistas/MenuView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoGrupo4.Vistas
+{
+    public partial class MenuView : Syncfusion.Windows.Forms.Office2010Form
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
